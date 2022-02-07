67eec51133253bed0924557bfbdcec12ab5792ec
CrioUser <crio-user@criodo.com>
2022-02-07 17:32:59 +0530

    ME_BUILDOUT_XBOARD_MODULE_XBOARD

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,95 +1,80 @@
-function getRssFeed(magazines) {
-
-    const accordionSection = document.getElementById('accordionSection');
-  
-      magazines.forEach((item, i) => {
-          async function getFeeds() {
-              try{
-                  const response = await fetch(`https://api.rss2json.com/v1/api.json?rss_url=${item}`);
-                  if (response.status === 200) {
-                      const data = await response.json();
-                      let id = ID();
-  
-                      const {feed, items} = data;
-  
-                      let accordion = renderAccordionSection(i, id, feed.title);
-                      accordionSection.innerHTML += accordion;
-                      
-                      renderCarouselItems(id, items);
-                  } else {
-                      throw error;
-                  }
-              } catch (err) {
-                  console.log(err);
-              }
-          }
-          getFeeds();
-      });
+let ID = () => Math.random().toString(36).substr(2, 9);
+let createAccordian = (title, id) => {
+  return `
+    <div class="accordion-item" id="card${id}">
+        <h2 class="accordion-header" id="heading${id}">
+            <button class="btn btn-link" aria-expanded="true" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${id}"  aria-controls="collapse${id}">            
+                ${title}
+            </button>
+        </h2>
+        <div id="collapse${id}" class="collapse" data-parent="#accordionId" aria-labelledby="heading${id}">
+        <div class="accordion-body">        
+      </div>
+        </div>
+    </div>
+    `;
+};
+let createCarouselOuter = (id, innerId) => {
+  return `
+    <div id="carouselControls${id}" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner" id="${innerId}">
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls${id}" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselControls${id}" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    `;
+};
+let createCarouselInner = (id, active) => {
+  return `
+    <div class="carousel-item ${active ? "active" : ""}" id="${id}">
+    </div>
+    `;
+};
+let createCard = (item) => {
+  return `
+    <div class="card d-block">
+        <img class="card-img-top img-fluid carousel-img" src="${item["enclosure"]["link"]}" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">${item["title"]}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${item["author"]}</h6>
+            <p class="card-subtitle text-secondary">${item["pubDate"]}</p>
+            <p class="card-text">${item["description"]}</p>
+            <a href="${item["link"]}" class="stretched-link" target="_blank"></a>
+        </div>
+    </div>`;
+};
+let addContent = async () => {
+  for (let i = 0; i < magazines.length; i++) {
+    let url = magazines[i];
+    let response = await fetch(
+      `https://api.rss2json.com/v1/api.json?rss_url=${encodeURI(url)}`
+    );
+    let data = await response.json();
+    console.log(data)
+    let accordionId = ID();
+    let accordion = createAccordian(data["feed"]["title"], accordionId);
+    document.getElementById("accordionId").innerHTML =document.getElementById("accordionId").innerHTML+ accordion;
+    if (i == 0) {
+      document.getElementById(`collapse${accordionId}`).classList.add("show");
+    }
+    let carouselId = ID();
+    let carouselInnerId = ID();
+    let carousel = createCarouselOuter(carouselId, carouselInnerId);
+    document.getElementById(`collapse${accordionId}`).innerHTML = carousel;
+    let items = data["items"];
+    for (j in items) {
+      let card = createCard(items[j]);
+      let innerCarouselCardId = ID();
+      let innerCarouselCard = createCarouselInner(innerCarouselCardId, j == 0);
+      document.getElementById(`${carouselInnerId}`).innerHTML += innerCarouselCard;
+      document.getElementById(`${innerCarouselCardId}`).innerHTML += card;
+    }
   }
-  
-  // random ID generator
-  let ID = () => Math.random().toString(36).substr(2, 9);
-  
-  // To render accordion items into DOM
-  const renderAccordionSection = (index, id, title) => {
-    let divAccordion = `<div class="accordion-item">
-                          <button class="accordion-button ${index === 0 ? "" : "collapsed"}" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${id}" aria-expanded="true" aria-controls="collapse-${id}">
-                            ${title}
-                          </button>
-                          <div id="collapse-${id}" class="accordion-collapse collapse ${index === 0 ? "show": ""}" aria-labelledby="heading-${id}" data-bs-parent="#accordionSection">
-                            <div class="accordion-body">
-                              <div id="indicator-${id}" class="carousel slide " data-ride="carousel"> 
-                                <div class="carousel-inner" id="carousel-inner-${id}">
-                                </div>
-                                <div>
-                                <a class="carousel-control-prev" href="#indicator-${id}" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                              </a>
-                                </div>
-                                <div>
-                                <a class="carousel-control-next" href="#indicator-${id}" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                              </a>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>`;
-    return divAccordion;
-  }
-  
-  // TO render Carousel Item within accordion body
-  const renderCarouselItems = (id, items) => {
-    const carouselInnerId = document.querySelector(`#carousel-inner-${id}`);
-  
-    items.forEach((el, j) => {
-      let carouselTemplate = carouselItemTemplate(el, j);
-      carouselInnerId.innerHTML += carouselTemplate;
-    })
-  
-    console.log(carouselInnerId);
-  }
-  
-  // To render each carousel item card to DOM
-  const carouselItemTemplate = (el, j) => {
-    let cardTemplate = `<div class="carousel-item ${j === 0 ? "active" : ""}">
-                          <div class="card d-block">
-                              <img class="w-100 img img-responsive" src=${el.enclosure.link} alt="${el.guid}">
-                              <div class="card-body">
-                                  <h5 class="card-title">${el.title}</h5>
-                                  <p class="card-author">${el.author} <span class="ellipse"></span> ${new Date(el.pubDate).toLocaleDateString()}</p>
-                                  <p class="card-text">${el.description.slice(0,100)}...
-                                  <a href="${el.link}" class="card-link " target="_blank">View Details</a>
-                                  </p>
-                              </div>
-                          </div>
-                        </div>
-                        `;
-    return cardTemplate;
-  }
-  
-  export {getRssFeed};
-  
-  
+};
+addContent();
